d0ef7e7609fc1b3674825c4866a3b8c739eae9be
ENGAG3 Media <media@engag3.us>
2016-06-04 08:52:40 -0700

    added github headers. plus gpl2

--- wp-remove-crud.php
+++ wp-remove-crud.php
@@ -1,13 +1,30 @@
 <?php
-/**
- * Plugin Name: E3 Remove admin extras
- * Plugin URI: https://www.engag3.media
- * Description: Simple plugin to remove un-needed features from the WordPress admin
- * Version: 1.0.1
- * Author: ENGAG3 Media
- * Author URI: https://www.engag3.media
- * GitHub Plugin URI: engag3/wp-remove-crud
- * License: GPL2
+/*!
+  Plugin Name: E3 Remove admin extras
+  Plugin URI: https://github.com/engag3/wp-remove-crud
+  Description: Simple plugin to remove un-needed features from the WordPress admin
+  Version: 1.0.1
+  Author: ENGAG3 Media
+  Author URI: https://www.engag3.media
+  GitHub Plugin URI: https://github.com/engag3/wp-remove-crud
+  GitHub Branch: master
+
+
+  Copyright 2016  ENGAG3 (email : media@engag3.us)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 
